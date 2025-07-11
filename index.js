fea19bb69c282488acfcb55559af5648d4261ccc
prince <pk20287022@gmail.com>
2025-07-11 15:36:26 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello, World!");
+// console.log(x);
+
+//  console.log(y);
+// console.log(z);
+
+// var x = 6
+// let y = 5;
+// const z = 4;
+
+// sayHi()
+// function sayHi () {
+//     console.log("Hello");
+// }
+
+
+
+
+// const name ={
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     fullName: function() {
+//        return this;
+//     }
+// }
+//  console.log(name.fullName());
+// function A(){
+//     console.log(this);   
+// }
+// A()
+
+
+
+const abc={
+    name :"Prince",
+    age: 25,
+    sayName: function() {
+        console.log(this.name);
+    }
+}
+abc.sayName()
